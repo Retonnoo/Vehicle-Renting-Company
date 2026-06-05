a0163ff253d40a51483d36a4aa38d33d28b9016d
Mohammad Nader Almasri <naderalmasri3@gmail.com>
2026-06-05 08:16:21 +0300

    Update IndividualClient.java
    class done

--- IndividualClient.java
+++ IndividualClient.java
@@ -2,4 +2,50 @@ package com.mycompany.main;
 
 public class IndividualClient extends Client {
 
+    private String drivingLicens;
+    private String birthDate;
+
+    public IndividualClient(int clientId, String clientName, String address, String phone, String drivingLicens, String birthDate) {
+        super(clientId, clientName, address, phone);
+        this.drivingLicens = drivingLicens;
+        this.birthDate = birthDate;
+    }
+
+    public String getDrivingLicens() {
+        return drivingLicens;
+    }
+
+    public void setDrivingLicens(String drivingLicens) {
+        this.drivingLicens = drivingLicens;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public double getDiscount(){
+        return 0;
+    }
+
+    @Override
+    public boolean isIndividual(){
+        return true;
+    }
+
+
+    @Override
+    public String toString() {
+        return "IndividualClient{" +
+                "drivingLicens='" + drivingLicens + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                '}';
+    }
 }
+
+
+
