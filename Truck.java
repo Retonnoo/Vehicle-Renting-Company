0b446b336636fb83d0d4e3c1229dfb0f572a67b1
Retonnoo <152440417+Retonnoo@users.noreply.github.com>
2026-06-06 21:58:22 +0300

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -1,5 +1,34 @@
 package com.mycompany.main;
 
 public class Truck extends Vehicle {
+//    Attributes :
+
+    private double cargoCapacity;
+    private boolean refrigerated;
+
+//    Constructor :
+    public Truck(double cargoCapacity, boolean refrigerated, String plateNum, String brand, String model, float dailyPrice, boolean availability) {
+        super(plateNum, brand, model, dailyPrice, availability);
+        this.cargoCapacity = cargoCapacity;
+        this.refrigerated = refrigerated;
+    }
+
+//    Setters And Getters :
+    public double getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(double cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public boolean isRefrigerated() {
+        return refrigerated;
+    }
+
+    public void setRefrigerated(boolean refrigerated) {
+        this.refrigerated = refrigerated;
+    }
 
 }
+
