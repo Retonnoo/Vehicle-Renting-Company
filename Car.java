b4bdf3e87b2491bda1ce4f8cea787f6fe03dcc85
Retonnoo <152440417+Retonnoo@users.noreply.github.com>
2026-06-06 21:56:07 +0300

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,5 +1,43 @@
 package com.mycompany.main;
 
 public class Car extends Vehicle {
-system.out.println("Hello Nader");
+
+//    Attributes :
+    private int seats;
+    private String fuelType;
+    private boolean airConditioned;
+
+//    Constructor :
+    public Car(int seats, String fuelType, boolean airConditioned, String plateNum, String brand, String model, float dailyPrice, boolean availability) {
+        super(plateNum, brand, model, dailyPrice, availability);
+        this.seats = seats;
+        this.fuelType = fuelType;
+        this.airConditioned = airConditioned;
+    }
+
+    //    Setters And Getters :
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public boolean isAirConditioned() {
+        return airConditioned;
+    }
+
+    public void setAirConditioned(boolean airConditioned) {
+        this.airConditioned = airConditioned;
+    }
+
 }
